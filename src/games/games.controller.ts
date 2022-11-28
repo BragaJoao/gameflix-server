616ac6459e484a2f5168abb5683079cf9fc77048
BragaJoao <joaogbraga@hotmail.com.br>
2022-11-28 08:26:37 -0300

    feate(games): create endpoint

--- src/games/games.controller.ts
+++ src/games/games.controller.ts
@@ -1,10 +1,15 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Post } from "@nestjs/common";
 
 @Controller('games')
 export class GamesController {
   @Get()
   findAll(){
     return 'Search for all games.'
+  }
+
+  @Post()
+  create(){
+    return 'Create new game.'
   }
 
 }
