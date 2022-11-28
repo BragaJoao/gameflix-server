a265d9513bfcedec0f6cf38f822247ef0597807d
BragaJoao <joaogbraga@hotmail.com.br>
2022-11-28 09:21:01 -0300

    feat(games): implement class-validator

--- src/games/dto/create-game.dto.ts
+++ src/games/dto/create-game.dto.ts
@@ -1,9 +1,26 @@
+import { IsNumber, IsPositive, IsString } from "class-validator";
+
 export class CreateGameDto {
+  @IsString()
   title: string;
+
+  @IsString()
   coverImageUrl: string;
+
+  @IsString()
   description: string;
+
+  @IsNumber()
+  @IsPositive()
   year: number;
+
+  @IsPositive()
+  @IsNumber()
   imdbScore: number;
+
+  @IsString()
   trailerYouTubeUrl: string;
+
+  @IsString()
   gameplayYouTubeUrl: string;
 }
