53b1f53ea6d6f9da72a8cec16de24e05b6f5e812
BragaJoao <joaogbraga@hotmail.com.br>
2022-11-28 20:00:43 -0300

    feat games repository

---
+++ src/games/games.repository.ts
@@ -0,0 +1,38 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { PartialGameDto } from "./dto/partialGameInput.dto";
+import { IGameEntity } from "./entities/game.entity";
+
+@Injectable()
+
+export class GameRepository {
+  constructor(private readonly prisma: PrismaService){}
+
+  async findAllGames():Promise<IGameEntity[]>{
+    const allGames = await this.prisma.games.findMany();
+    return allGames
+  }
+
+  async findOneGame(id: string):Promise<IGameEntity>{
+    const foundGame = await this.prisma.games.findUniqueOrThrow({where: {id: id}});
+    return foundGame
+  }
+
+  async createGame(game: IGameEntity): Promise<IGameEntity>{
+    const CreatedGame = await this.prisma.games.create({ data: game});
+    return CreatedGame
+  }
+
+  async updateGame(game: PartialGameDto): Promise<IGameEntity>{
+    const updatedGame = await this.prisma.games.update({
+      where: {id: game.id},
+      data: game,
+    });
+    return updatedGame
+  }
+
+ async deleteGame (id:string): Promise<IGameEntity>{
+  const deletedGame = await this.prisma.games.delete({ where: {id: id}});
+  return deletedGame
+ }
+}
