e8011302e45d19c81667a3418ca53ec0972444a5
BragaJoao <joaogbraga@hotmail.com.br>
2022-12-13 21:43:58 -0300

    feat(auth): implement strategy and guards

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, HttpCode, HttpStatus, Post, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiOperation, ApiTags  } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { LoginResponseDto } from './dto/login-response.dto';
 import { LoginDto } from './dto/login.dto';
@@ -13,5 +14,15 @@ export class AuthController {
   @HttpCode(HttpStatus.OK)
   login(@Body() loginDto: LoginDto):Promise<LoginResponseDto> {
     return this.authService.login(loginDto);
+  }
+
+  @Get()
+  @UseGuards(AuthGuard())
+  @ApiOperation({
+    summary: 'Returns authenticated user'
+  })
+  @ApiBearerAuth()
+  profile(){
+    return 'Successfully authenticated'
   }
 }
